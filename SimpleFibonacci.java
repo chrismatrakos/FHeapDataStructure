359100f09cb60d4e83627ef9dd592448d778ffb8
chrismatrakou <chrismatrakou@gmail.com>
2021-11-09 10:05:18 +0100

    Update SimpleFibonacci.java

--- SimpleFibonacci.java
+++ SimpleFibonacci.java
@@ -1,16 +1,6 @@
 import java.util.ArrayList;
 import java.lang.StringBuilder;
 
-/*
- * @author Chris Matrakou, Remy Themsen.
- * Both Students of ITU Copenhagen Spring 2016.
- * 
- * This is a priority queue implementation of the simple fibonacci heap as
- * described in the paper "Fibonacci Heaps Revisited" By Haim Kaplan, Robert E. 
- * Tarjan and Uri Zwick, 2014.
- *
- */
-
 public class SimpleFibonacci {
 	// Map used for decrease specific Key and contains
 	private Node[] nodeMap;
